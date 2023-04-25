dac42a838ed7851487385170e8147460d7c1a4d4
beata098 <Betty2001>
2023-04-25 16:52:26 +0200

    lab 2

--- Calculator.g4
+++ Calculator.g4
@@ -1,10 +1,13 @@
 grammar Calculator;
  
-expression: integralExpression ((PLUS | MINUS) integralExpression)*;
+expression: integralExpression ((PLUS | MINUS) multiplyingExpression)*;
+multiplyingExpression: integralExpression ((MULT|DIV) multiplyingExpression)*;
 integralExpression: MINUS INT | INT;
  
 INT: [0-9]+ ;
 PLUS: '+' ;
 MINUS: '-' ;
+MULT: '*';
+DIV: '/';
 INTEGRAL: 'cal';
 WS : [ \t\r\n]+ -> skip ;
--- CalculatorMainListener.java
+++ CalculatorMainListener.java
@@ -1,11 +1,60 @@
+import com.sun.jdi.IntegerValue;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Objects;
+
 public class CalculatorMainListener extends CalculatorBaseListener{
 
+    Deque<Integer> numbers = new ArrayDeque<>();
+
+
+    /*@Override
+    public void enterExpression(CalculatorParser.ExpressionContext ctx) {
+        System.out.println("enterExpression:"+ctx.getText());
+        super.enterExpression(ctx);
+    }*/
+
+    @Override
+    public void exitExpression(CalculatorParser.ExpressionContext ctx) {
+        System.out.println("exitExpression:"+ctx.getText());
+
+        Integer value = numbers.pop();
+        for (int i = 1; i < ctx.getChildCount(); i=i+2){
+            if (Objects.equals(ctx.getChild(i).getText(), "+")){
+                value = value + numbers.pop();
+            } else {
+                value = value - numbers.pop();
+            }
+
+        }
+        numbers.add(value);
+        super.exitExpression(ctx);
+    }
+
+    /*@Override
+    public void enterIntegralExpression(CalculatorParser.IntegralExpressionContext ctx) {
+        System.out.println("enterIntegralExpression:"+ctx.getText());
+        super.enterIntegralExpression(ctx);
+    }*/
+
+    @Override
+    public void exitIntegralExpression(CalculatorParser.IntegralExpressionContext ctx) {
+        System.out.println("exitIntegralExpression:"+ctx.getText());
+        if (ctx.MINUS() != null){
+            numbers.add(-1 * Integer.valueOf(ctx.INT().toString()));
+        } else {
+            numbers.add(Integer.valueOf(ctx.INT().toString()));
+        }
+        super.exitIntegralExpression(ctx);
+    }
+
+
     public static void main(String[] args) throws Exception {
-        CharStream charStreams = CharStreams.fromFileName("./example.txt");
-        Integer result = calc(charStreams);
+        //CharStream charStreams = CharStreams.fromFileName("./example.txt");
+        Integer result = calc("1+ -2 + -3");
         System.out.println("Result = " + result);
     }
 
@@ -27,6 +76,6 @@ public class CalculatorMainListener extends CalculatorBaseListener{
     }
 
     private Integer getResult() {
-        return 0;
+        return numbers.peek();
     }
 }
